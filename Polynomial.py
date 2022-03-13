33574291d49187460f4375861f7f0bdaa28e0fc4
battar11 <85445331+battar11@users.noreply.github.com>
2022-03-13 21:51:00 +0300

    Create Polynomial.py

---
+++ Polynomial.py
@@ -0,0 +1,93 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+
+
+#Download the data set 
+
+df = pd.read_csv("FuelConsumption.csv")
+
+# take a look at the dataset
+df.head()
+
+#Selecting the features we want to use
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+
+#Plot the data 
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+#Using split/ test apporche and we have deicied to use 80% for traning and 20% for testing the data 
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+
+# so here will be converting non-linear to linear regression 
+#However it's kind of linear because the value of Beta is having non-polynomial 
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+
+
+poly = PolynomialFeatures(degree=2)
+train_x_poly = poly.fit_transform(train_x)
+
+
+# Now we deal with it as linear regression 
+clf = linear_model.LinearRegression()
+train_y_ = clf.fit(train_x_poly, train_y)
+# The coefficients
+print ('Coefficients: ', clf.coef_)
+print ('Intercept: ',clf.intercept_)
+
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+XX = np.arange(0.0, 10.0, 0.1)
+yy = clf.intercept_[0]+ clf.coef_[0][1]*XX+ clf.coef_[0][2]*np.power(XX, 2)
+plt.plot(XX, yy, '-r' )
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+
+
+#Evaluation 
+from sklearn.metrics import r2_score
+
+test_x_poly = poly.transform(test_x)
+test_y_ = clf.predict(test_x_poly)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y,test_y_ ) )
+
+
+#practice 
+# write your code here
+poly3 = PolynomialFeatures(degree=3)
+train_x_poly3 = poly3.fit_transform(train_x)
+clf3 = linear_model.LinearRegression()
+train_y3_ = clf3.fit(train_x_poly3, train_y)
+
+# The coefficients
+print ('Coefficients: ', clf3.coef_)
+print ('Intercept: ',clf3.intercept_)
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+XX = np.arange(0.0, 10.0, 0.1)
+yy = clf3.intercept_[0]+ clf3.coef_[0][1]*XX + clf3.coef_[0][2]*np.power(XX, 2) + clf3.coef_[0][3]*np.power(XX, 3)
+plt.plot(XX, yy, '-r' )
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+test_x_poly3 = poly3.transform(test_x)
+test_y3_ = clf3.predict(test_x_poly3)
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y3_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y3_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y,test_y3_ ) )
